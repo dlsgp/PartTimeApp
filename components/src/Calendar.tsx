0d4242426c71067fda5253d6a38d293d0c5e4dc2
dlsgp <khyioh4413@gmail.com>
2024-07-17 20:07:18 +0900

    App start

---
+++ components/src/Calendar.tsx
@@ -0,0 +1,75 @@
+import { StatusBar } from "expo-status-bar";
+import DatePicker from "react-native-modern-datepicker";
+import { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, Modal } from "react-native";
+
+export default function TabOneScreen() {
+  const [open, setOpen] = useState(false); // open and closes the modal
+  const [date, setDate] = useState<string | null>(null); // date variable
+
+  function handleOnPress() {
+    setOpen(!open);
+  }
+
+  function handleChange(propDate: string) {
+    setDate(propDate);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>App color</Text>
+
+      <TouchableOpacity onPress={handleOnPress}>
+        <Text>Open</Text>
+      </TouchableOpacity>
+
+      <Modal animationType="slide" transparent={true} visible={open}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <DatePicker
+              mode="calendar"
+              selected={date || ""}
+              onDateChanged={handleChange}
+            />
+
+            <TouchableOpacity onPress={handleOnPress}>
+              <Text>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  centeredView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    width: "90%",
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
