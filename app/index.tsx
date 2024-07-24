1c8bc789b56e6943328ba61b3b917484a75e3bf9
kwonsunhyeon <topsh90@gmail.com>
2024-07-24 15:04:51 +0900

    앱 메인화면 수정

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,10 @@ import { StackActions } from "@react-navigation/native";
 import { useNavigation } from "expo-router";
 import { Button, Text } from "react-native";
 import { View } from "react-native";
+import SignInApp from "./(signIn)/SignInApp";
 
 const MainPage = () => {
-  return (
-    <View>
-      <Text>Main</Text>
-    </View>
-  );
+  return <SignInApp></SignInApp>;
 };
 
 export default MainPage;
